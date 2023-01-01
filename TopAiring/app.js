3fa91ad99f7c14ed88e9c569efdbaa2400f1ee81
sougata das <sougata19582001@gmail.com>
2023-01-01 20:58:18 +0530

    added Top Airing Section

---
+++ TopAiring/app.js
@@ -0,0 +1,59 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd615a14e2cmsh37898fcd6929e18p13462ejsn334a6edfc023',
+		'X-RapidAPI-Host': 'myanimelist.p.rapidapi.com'
+	}
+};
+
+fetch('https://myanimelist.p.rapidapi.com/anime/top/airing', options)
+	.then(response => response.json())
+	.then(response => {
+        
+        console.log(response)
+        for (let i = 0; i < response.length; i++){
+            // create elements
+            const col = document.createElement("div")
+            const card = document.createElement("div")
+            const cardImage = document.createElement("img")
+            const cardBody = document.createElement("div")
+            const cardTitle = document.createElement("h5")
+            const airedOn = document.createElement("p")
+            const animeLink = document.createElement("a")
+            // assign class, id, attributes
+            col.classList.add("col")
+            card.classList.add("card")
+            card.classList.add(`c-${i}`)
+            card.style.width = "18rem"
+            card.style.height = "38rem"
+            cardImage.classList.add("card-img-top")
+            cardImage.classList.add(`img-${i}`)
+            cardImage.classList.add('img-fluid')
+            cardImage.style.height = '25rem'
+            cardImage.src = response[i].picture_url.replace('/r/50x70', '')
+            cardBody.classList.add("card-body")
+            cardTitle.classList.add("card-title")
+            cardTitle.classList.add(`title-${i}`)
+            cardTitle.textContent = response[i].title
+            airedOn.classList.add("card-text")
+            airedOn.classList.add(`aired-on-${i}`)
+            airedOn.textContent = `Aired On : ${response[i].aired_on}`
+            animeLink.classList.add("btn")
+            animeLink.classList.add("btn-primary")
+            animeLink.classList.add(`anime-link-${i}`)
+            animeLink.id = "link"
+            animeLink.target = "_blank"
+            animeLink.rel = "noopener noreferrer"
+            animeLink.href = response[i].myanimelist_url
+            animeLink.innerHTML = `Know More <i class="fa-solid fa-arrow-up-right-from-square"></i>`
+            // append the elements
+            document.querySelector(".row").appendChild(col)
+            col.appendChild(card)
+            card.appendChild(cardImage)
+            card.appendChild(cardBody)
+            cardBody.appendChild(cardTitle)
+            cardBody.appendChild(airedOn)
+            cardBody.appendChild(animeLink)
+        }
+    })
+	.catch(err => console.error(err));
